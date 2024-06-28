4519fb6873d440578ee4d69cefa7107e61382e9b
Jorn Boerema <jorn@bijvoorbeeldzo.nl>
2024-06-28 15:42:05 +0200

    fixed the error before the migrations are runned

--- src/BzCMSServiceProvider.php
+++ src/BzCMSServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace JornBoerema\BzCMS;
 
+use Illuminate\Support\Facades\Schema;
 use JornBoerema\BzCMS\Commands\CreateCMSBlockCommand;
 use JornBoerema\BzUserManagement\Livewire\AcceptInvitation;
 use Livewire\Livewire;
@@ -24,12 +25,17 @@ class BzCMSServiceProvider extends PackageServiceProvider
             ->hasInstallCommand(function (InstallCommand $installCommand) {
                 $installCommand->publishMigrations();
                 $installCommand->publishConfigFile();
+                $installCommand->askToRunMigrations();
             });
     }
 
     public function boot()
     {
-        parent::boot();
+        $this->app->booted(function () {
+            if(Schema::hasTable('pages') && Schema::hasTable('navigations')) {
+                parent::boot();
+            }
+        });
 
         Livewire::component('jorn-boerema.bz-cms.livewire.page_template', AcceptInvitation::class);
     }
