50c5dd8e8872d9b71eadd037a0807a976ee878d6
Jorn Boerema <jorn@bijvoorbeeldzo.nl>
2024-07-10 15:59:04 +0200

    added dutch translations

--- resources/lang/nl/bz-cms.php
+++ resources/lang/nl/bz-cms.php
@@ -1,5 +1,26 @@
 <?php
 
 return [
-    
+    // Global
+    'created_at' => 'Gemaakt Datum',
+    'updated_at' => 'Laatst Aangepast Datum',
+    'title' => 'Titel',
+
+    // Navigation
+    'name' => 'Naam',
+    'items' => 'Items',
+    'type' => 'Type',
+    'url' => 'URL',
+    'page' => 'Pagina',
+    'children' => 'Children',
+    'types' => [
+        'page' => 'Pagina',
+        'dropdown' => 'Dropdown',
+        'external' => 'Extern',
+    ],
+
+    // Page
+    'slug' => 'Slug',
+    'elements' => 'Elementen'
+
 ];
